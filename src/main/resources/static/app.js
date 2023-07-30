c9527ca8bb880a70a699d8dbf3de2ee7623eb8ca
trung.dang <kokonut.hustle@gmail.com>
2023-07-31 05:14:17 +0700

    try to deploy on heroku

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket("ws://localhost:8080/ws");
+const socket = new WebSocket("wss://virtual-file-system-f3afa8f866d4.herokuapp.com/ws");
 
 var term = new window.Terminal({
     cursorBlink: true
